97b87d036b110138f8cc6a37e9796ebc7d68b4eb
Xuanzhao Gao <xgao1@ccmlin078.flatironinstitute.org>
2025-11-10 19:33:35 -0500

    update results

--- pdmk/runtime/vary_number.jl
+++ pdmk/runtime/vary_number.jl
@@ -8,45 +8,52 @@ Random.seed!(1234)
 function main()
 
     filename = "pdmk_runtime_float.csv"
-    CSV.write(filename, DataFrame(N = Int[], L = Float64[], time_propose = Float64[], time_accept = Float64[]))
+    # CSV.write(filename, DataFrame(N = Int[], L = Float64[], ns = Int[], n_levels = Int[], time_propose = Float64[], time_accept = Float64[]))
 
-    Ns = [10000, 12689, 16102, 20433, 25929, 32903, 41753, 52983, 67233, 85316, 108263, 137382, 174332, 221221, 280721, 356224, 452036, 573615, 727895, 923670, 1172102, 1487352, 1887391, 2395026, 3039195, 3856620, 4893900, 6210169, 7880462, 10000000]
+    # Ns = [10000, 12689, 16102, 20433, 25929, 32903, 41753, 52983, 67233, 85316, 108263, 137382, 174332, 221221, 280721, 356224, 452036, 573615, 727895, 923670, 1172100, 1487352, 1887391, 2395026, 3039195, 3856622, 4893900, 6210169, 7880462, 10000000]
+    Ns = [3856622, 4893900, 6210169, 7880462, 10000000]
+    # nss = [100:50:300...]
+    nss = [200, 300]
 
     for N in Ns
-        println("Running for N = $N")
-        L = Float32(3.0 * ( N^(1.0/3.0)))
+        for ns in nss
+            println("Running for N = $N, ns = $ns")
+            L = Float32(3.0 * ( N^(1.0/3.0)))
 
-        coords = rand(Float32, 3, N) .* L
-        charges = randn(Float32, N)
-        charges .-= sum(charges) / N
+            coords = rand(Float32, 3, N) .* L
+            charges = randn(Float32, N)
+            charges .-= sum(charges) / N
 
-        params = PDMK4MC.HPDMKParams(L = L, n_per_leaf=200, init = PROXY)
-        tree = PDMK4MC.create_tree(coords, charges; params=params)
+            params = PDMK4MC.HPDMKParams(L = L, n_per_leaf=ns, init = PROXY)
+            tree = PDMK4MC.create_tree(coords, charges; params=params)
 
-        # warm up
-        eval_shift_energy(tree, 1, 0.1f0, 0.1f0, 0.1f0)
-        update_shift!(tree, 1, 0.1f0, 0.1f0, 0.1f0)
+            n_levels = tree_depth(tree)
 
-        time_propose = 0.0
-        time_accept = 0.0
+            # warm up
+            eval_shift_energy(tree, 1, 0.1f0, 0.1f0, 0.1f0)
+            update_shift!(tree, 1, 0.1f0, 0.1f0, 0.1f0)
 
-        n_trials = 10000
-        for i in 1:n_trials
+            time_propose = 0.0
+            time_accept = 0.0
 
-            idx = rand(1:N)
-            dx = Float32(0.25 * randn())
-            dy = Float32(0.25 * randn())
-            dz = Float32(0.25 * randn())
+            n_trials = 10000
+            for i in 1:n_trials
 
-            time_propose += @elapsed eval_shift_energy(tree, idx, dx, dy, dz)
-            time_accept += @elapsed update_shift!(tree, idx, dx, dy, dz)
+                idx = rand(1:N)
+                dx = Float32(0.25 * randn())
+                dy = Float32(0.25 * randn())
+                dz = Float32(0.25 * randn())
+
+                time_propose += @elapsed eval_shift_energy(tree, idx, dx, dy, dz)
+                time_accept += @elapsed update_shift!(tree, idx, dx, dy, dz)
+            end
+
+            destroy_tree!(tree)
+
+            println("Time propose: $(time_propose / n_trials), Time accept: $(time_accept / n_trials)")
+
+            CSV.write(filename, DataFrame(N = N, L = L, ns = ns, n_levels = n_levels,time_propose = time_propose / n_trials, time_accept = time_accept / n_trials), append=true)
         end
-
-        destroy_tree!(tree)
-
-        println("Time propose: $(time_propose / n_trials), Time accept: $(time_accept / n_trials)")
-
-        CSV.write(filename, DataFrame(N = N, L = L, time_propose = time_propose / n_trials, time_accept = time_accept / n_trials), append=true)
     end
 end
 
--- simulation/1_1_salt/1_1_salt_mc.jl
+++ simulation/1_1_salt/1_1_salt_mc.jl
@@ -1,11 +1,25 @@
-using Molly
+using Molly, PDMK4MC
 
-n_atoms = 330
-atoms = [Atom(mass=1.0u"g/mol", charge=0.0, σ=0.5u"nm", ϵ=1.0u"kJ/mol") for i in 1:n_atoms]
+n_atoms = 1000
+
+charges = randn(n_atoms)
+charges .-= sum(charges) / n_atoms
+
+atoms = [Atom(mass=1.0u"g/mol", charge=charges[i], σ=0.5u"nm", ϵ=1.0u"kJ/mol") for i in 1:n_atoms]
 boundary = CubicBoundary(30.0u"nm", 30.0u"nm", 30.0u"nm")
 
 coords = place_atoms(n_atoms, boundary; min_dist=1.0u"nm")
 pairwise_inters = (LennardJones(cutoff=DistanceCutoff(1.122462048309373u"nm"), shift = true),)
+
+coords_val = zeros(3, n_atoms)
+for i in 1:n_atoms
+    for j in 1:3
+        coords_val[j, i] = coords[i][j].val
+    end
+end
+
+params = PDMK4MC.HPDMKParams(L = 30.0, digits = 3, n_per_leaf = 10, init = PDMK4MC.DIRECT)
+tree = PDMK4MC.create_tree(coords_val, charges; params = params)
 
 sys = System(
     atoms=atoms,
@@ -19,13 +33,15 @@ sys = System(
 )
 
 trial_args = Dict(:shift_size => 0.3u"nm")
-sim = MetropolisMonteCarlo(;
+sim = MetropolisMonteCarloPDMK(;
     temperature=300.0u"K",
-    trial_moves=random_uniform_translation!,
+    trial_moves=random_uniform_translation,
     trial_args=trial_args,
+    tree=tree,
 )
 
 simulate!(sys, sim, 10_000)
+PDMK4MC.destroy_tree!(tree)
 
 println(sys.loggers.montecarlo.n_accept)
 
